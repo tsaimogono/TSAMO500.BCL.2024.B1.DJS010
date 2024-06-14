f465f7e725e669fe070029bcecd41d2f7fd564c1
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-06-14 23:48:02 +0200

    fetched from api

--- src/utils.js/fetchPosts.js
+++ src/utils.js/fetchPosts.js
@@ -6,3 +6,13 @@ export const useFetchPosts = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  
+  // effect hook to fetch posts when component mounts
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setIsLoading(true)
+      try {
+        // Fetch posts from API
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
